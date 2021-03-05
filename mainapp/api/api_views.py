549ce5dad6b0c21f6a5883e112238dde987afb8d
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-03-05 22:28:19 +0200

    6 commit

--- mainapp/api/api_views.py
+++ mainapp/api/api_views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import AuthorSerializer, CommentSerializer, NewsPostSerializer
 from ..models import Author, NewsPost, Comment
@@ -12,6 +14,13 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class NewsPostViewSet(viewsets.ModelViewSet):
     serializer_class = NewsPostSerializer
     queryset = NewsPost.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def upvote(self, request, pk=None):
+        post = self.get_object()
+        post.amount_of_upvotes += 1
+        post.save()
+        return Response({'status': 'post upvoted'})
 
 
 class CommentViewSet(viewsets.ModelViewSet):
